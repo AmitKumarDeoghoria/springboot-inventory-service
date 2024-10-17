94a7d880fe60afab644baab4e1899ac0db43398a
Amit Kumar Deohoria <kamit5741@gmail.com>
2024-10-17 21:52:34 +0530

    Addition and Substraction func added

--- src/main/java/com/microservices/inventory/git/gitpractice.java
+++ src/main/java/com/microservices/inventory/git/gitpractice.java
@@ -1,15 +1,26 @@
 package com.microservices.inventory.git;
 
 public class gitpractice {
+
+    public static int sum(int a1, int a2)
+    {
+        return a1+a2;
+    }
+    public static int sub(int a1, int a2)
+    {
+        return a1-a2;
+    }
     public  static void main(String[]args)
     {
 
         System.out.println("Hey I am added again");
         int a=2;
         int b=3;
-        int sum=a+b;
-        System.out.println(a+b);
-        System.out.println(a-b);
+
+
+
+        System.out.println(sum(a,b));
+        System.out.println(sub(a,b));
         System.out.println(a*b);
         System.out.println(a/b);
         System.out.println("Hey This is from main branch");
