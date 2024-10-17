a488a58ecf1b1de48308a562847a7e0b652b3d51
Amit Kumar Deohoria <kamit5741@gmail.com>
2024-10-17 18:33:00 +0530

    dckc

--- src/main/java/com/microservices/inventory/git/gitpractice.java
+++ src/main/java/com/microservices/inventory/git/gitpractice.java
@@ -12,6 +12,7 @@ public class gitpractice {
         System.out.println(a-b);
         System.out.println(a*b);
         System.out.println(a/b);
+        System.out.println("hello this is the final commit");
 
     }
 
