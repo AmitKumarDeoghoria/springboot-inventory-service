dc3b8fe34e2b4c2ab40d8f6abf5499edac84d132
Amit Kumar Deohoria <kamit5741@gmail.com>
2024-10-17 22:15:37 +0530

    bug2 fixed

--- src/main/java/com/microservices/inventory/git/gitpractice.java
+++ src/main/java/com/microservices/inventory/git/gitpractice.java
@@ -36,8 +36,7 @@ public class gitpractice {
 
         System.out.println(sum(a,b));
         System.out.println(sub(a,b));
-        System.out.println(a*b);
-        System.out.println(a/b);
+
 
         System.out.print("hello this is message from main");
 
