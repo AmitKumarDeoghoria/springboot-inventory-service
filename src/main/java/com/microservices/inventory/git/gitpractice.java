e792d19df5f020f10d6eb9e027a73d81400f46bc
Amit Kumar Deohoria <kamit5741@gmail.com>
2024-10-17 21:58:51 +0530

    Multiplication and Division Added

--- src/main/java/com/microservices/inventory/git/gitpractice.java
+++ src/main/java/com/microservices/inventory/git/gitpractice.java
@@ -1,6 +1,15 @@
 package com.microservices.inventory.git;
 
 public class gitpractice {
+
+    public static int mult(int a1 , int a2)
+    {
+        return a1*a2;
+    }
+    public static int div(int a1 , int a2)
+    {
+        return a1/a2;
+    }
     public  static void main(String[]args)
     {
 
@@ -10,8 +19,8 @@ public class gitpractice {
         int sum=a+b;
         System.out.println(a+b);
         System.out.println(a-b);
-        System.out.println(a*b);
-        System.out.println(a/b);
+        System.out.println(mult(a,b));
+        System.out.println(div(a,b));
         System.out.println("Hey This is from main branch");
         System.out.println("Hey main How are you? I am bug");
 
