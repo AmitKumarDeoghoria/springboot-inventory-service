3da08b297a281d8eef471ac4423a3b051a9d1195
Amit Kumar Deohoria <kamit5741@gmail.com>
2024-10-17 17:24:21 +0530

    New git prac file added

---
+++ src/main/java/com/microservices/inventory/git/gitpractice.java
@@ -0,0 +1,11 @@
+package com.microservices.inventory.git;
+
+public class gitpractice {
+    public  static void main(String[]args)
+    {
+
+        System.out.println("Hey I am added again");
+    }
+
+
+}
