e38b71ef143708755de66d29e4b0d8e882c4fd60
Amit Kumar Deohoria <kamit5741@gmail.com>
2024-10-17 18:12:07 +0530

    one var added

--- src/main/java/com/microservices/inventory/git/gitpractice.java
+++ src/main/java/com/microservices/inventory/git/gitpractice.java
@@ -7,10 +7,12 @@ public class gitpractice {
         System.out.println("Hey I am added again");
         int a=2;
         int b=3;
+        int sum=a+b;
         System.out.println(a+b);
         System.out.println(a-b);
         System.out.println(a*b);
         System.out.println(a/b);
+
     }
 
 
